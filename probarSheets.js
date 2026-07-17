f7b591879a0c1f1299949be1f9bd017f16d90a9b
JuanCarvajal28 <jmartincg28@gmail.com>
2026-07-16 22:31:21 -0500

    DELETE: Se elimino el archivo de prueba de Sheets de google. El bot ahora es capaz de editar la hoja de calculo tomando como punto inicial el mensaje enviado a whatsapp.

--- probarSheets.js
+++
@@ -1,8 +0,0 @@
-const { agregarGasto } = require('./sheets');
-
-agregarGasto({
-    fecha: '2026-07-16',
-    descripcion: 'Prueba desde código',
-    categoria: 'Otros',
-    valor: 5000
-}).then(() => console.log('¡Fila agregada con éxito!'));
